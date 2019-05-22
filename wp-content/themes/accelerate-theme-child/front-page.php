7f95e8f456c7e4dede7463badc4370af808e1c57
reggie <reggiekcollins@gmail.com>
2019-05-22 09:03:19 -0500

    updated files

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -19,24 +19,37 @@ get_header(); ?>
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
-				<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
+				<a class="button" href="<?php echo site_url('/case-studies/mailchimp-campaign/') ?>">View Our Work</a>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
-  <section class="recent-posts">
-    <div class="site-content">
-      <div class="blog-post">
-        <h4>From the Blog</h4>
+		<section class="featured-work">
+			<div class="site-content">
+					<h3>Featured Work</h3>
 
-        <?php query_posts('posts_per_page=1'); ?>
-        <?php while (have_posts()) : the_post(); ?>
+					<ul class="homepage-featured-work">
+					<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+					<?php while (have_posts()) : the_post();
+								$image_1 = get_field('image_1');
+								$image_2 = get_field('image_2');
+								$image_3 = get_field('image_3');
+								$size = "medium";
+					 ?>
+					 			<li class="individual-featured-work">
+							 			<figure>
+						 		<?php  echo wp_get_attachment_image($image_1, $size); ?>
+								<?php  echo wp_get_attachment_image($image_2, $size); ?>
+								<?php  echo wp_get_attachment_image($image_3, $size); ?>
+							 			</figure>
 
-        <h3><?php the_title(); ?></h3>
-        <?php the_excerpt(); ?>
-        <?php endwhile; ?>
-        <?php wp_reset_query(); ?>
-     </div>
-    </div>
-  </section>
+										<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+								</li>
+					<?php endwhile; ?>
+					<?php wp_reset_query(); ?>
+				</ul>
+			</div>
+		</section>
+
+
 <?php get_footer(); ?>
