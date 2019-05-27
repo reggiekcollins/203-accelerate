e79a7de17fbbc89d5d1852b7b1a5fdc55f3e081e
reggie <reggiekcollins@gmail.com>
2019-05-27 11:04:48 -0500

    updated front-page.php

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -57,17 +57,15 @@ get_header(); ?>
 					<?php the_excerpt(); ?>
 					<?php endwhile; ?>
 					<?php wp_reset_query(); ?>
-
-					<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
-					<div id="secondary" class="widget-area" role="complementary">
-						<?php dynamic_sidebar( 'sidebar-2' ); ?>
-					</div>
-					<?php endif; ?>
 			 </div>
+
+			 <?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+			 <div id="secondary" class="widget-area" role="complementary">
+				 <?php dynamic_sidebar( 'sidebar-2' ); ?>
+			 </div>
+			 <?php endif; ?>
+
 			</div>
 		</section>
-
-
-
 
 <?php get_footer(); ?>
